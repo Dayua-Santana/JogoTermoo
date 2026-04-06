023dd9a6029fef94a58ea5677621f6a67be91dfb
Dayuã-Santana <daniponsantana@gmail.com>
2026-04-06 18:51:30 -0300

    Criando loop da rodada e validando

--- Jogotermo/Program.cs
+++ Jogotermo/Program.cs
@@ -8,9 +8,70 @@ class Program
         ExibirCabecalho();
         string[] palavras = ["TESTE", "TRENS", "CASAS", "FOGAO", "MELAO", "UMBOS"];
         int indiceAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
-        Console.WriteLine(palavras[indiceAleatorio]);
+        string palavraAleatoria = palavras[indiceAleatorio];
+
+        int tentativas = 0;
+        bool acertou = false;
+
+        while (tentativas < 6 && !acertou)
+        {
+            Console.Write($"\nTentativa {tentativas + 1} /6: ");
+            string? chuteRaw = Console.ReadLine();
 
 
+            string chute = "";
+            if (chuteRaw == null || chuteRaw == "")
+            {
+                chute = "AAAAA";
+            }
+            else
+            {
+
+                for (int i = 0; i < chuteRaw.Length && i < 5; i++)
+                {
+                    chute += char.ToUpper(chuteRaw[i]);
+                }
+
+                while (chute.Length < 5)
+                {
+                    chute += 'A';
+                }
+            }
+
+            string resultado = "";
+            for (int indice = 0; indice < 5; indice++)
+            {
+                char letraChute = chute[indice];
+                char letraCorreta = palavraAleatoria[indice];
+
+                if (letraChute == letraCorreta)
+                    resultado += "🟩";
+                else if (palavraAleatoria.Contains(letraChute))
+                    resultado += "🟨";
+                else
+                    resultado += "🔴";
+            }
+            Console.WriteLine($"{chute} --> {resultado}");
+            tentativas++;
+            acertou = (resultado == "🟩🟩🟩🟩🟩");
+
+            if (acertou)
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine($"🎉VOCÊ GANHOU! A palavra era {palavraAleatoria}");
+                Console.WriteLine("--------------------------------------------");
+                Console.ReadLine();
+                return;
+            }
+        }
+
+        if (!acertou)
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine($"💀GAME OVER! A palavra era {palavraAleatoria}");
+            Console.WriteLine("-----------------------------------------------");
+            Console.ReadLine();
+        }
 
 
     }
